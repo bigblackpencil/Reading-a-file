551e06cbcfc828c669eaed0d08ffeb112070b1ac
IgrekChkur <zagutin322@gmail.com>
2023-11-04 14:55:56 +0300

    first version

---
+++ Read File.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    int size;
+    std::cout << "Enter a size of your array: ";
+    std::cin >> size;
+    int* Array = new int[size];
+
+    std::ifstream readFile("E:\\C++\\Zhegalkin-polynomial\\BoolFunctions.txt");
+    if (!readFile.is_open()) 
+    {
+        std::cerr << "Error." << std::endl;
+        return 1;
+    }
+
+    // Чтение файла построчно и обработка
+    std::string line;
+    while (std::getline(readFile, line))
+    {
+        //Пропускаем пустые строки, если ими раздедены сторки в файле
+        if (line == "") continue;
+        // Пропускаем пробелы и запятые из строки
+        for (int i = 0, j = 0; j < size; i++) 
+        {
+            char c = line[i];
+
+            if (c == ' ' || c == ',') continue;
+
+            Array[j] = c - '0';
+            std::cout << Array[j] << ' ';
+            j++;
+        }
+        std::cout << '\n';
+    }
+
+    readFile.close();
+
+	return 0;
+}
+
